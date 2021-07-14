52a4b2c1c14ef8811f0ebf33cdaa060487cd4080
lesly <lkpsmx@gmail.com>
2021-07-14 18:14:01 -0500

    chore: refactor

---
+++ src/exports/variables.js
@@ -0,0 +1,25 @@
+const score0El = document.getElementById("score--0");
+const score1El = document.getElementById("score--1");
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
+
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+
+
+export {
+  score0El,
+  score1El,
+  player0El,
+  player1El,
+  current0El,
+  current1El,
+  diceEl,
+  btnNew,
+  btnRoll,
+  btnHold,
+};
